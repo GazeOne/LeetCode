67a17111f2c8d7a95648d4c1d5ae147774fa2275
GazeOne <1198861863@qq.com>
2020-10-18 19:03:55 +0800

    【提交类型】：35 77

--- src/_70ClimbStairs.java
+++ src/_70ClimbStairs.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class _70ClimbStairs {
 
     public int climbStairs(int n) {
@@ -13,5 +15,79 @@ public class _70ClimbStairs {
         }
         return result;
     }
+
+    public double trimMean(int[] arr) {
+
+        if (arr == null) return 0;
+
+        int size = arr.length;
+        int deleteSize = size * 5 / 100;
+        deleteSize = (deleteSize == 0 ? deleteSize : deleteSize);
+
+        Arrays.sort(arr);
+        double sum = 0.0;
+        double count = 0.0;
+        for (int i = deleteSize; i < size - deleteSize; i++) {
+            sum += arr[i];
+            count++;
+        }
+        return sum / count;
+    }
+
+    public int[] bestCoordinate(int[][] towers, int radius) {
+
+        int minI = Integer.MAX_VALUE;
+        int maxI = Integer.MIN_VALUE;
+        int minJ = Integer.MAX_VALUE;
+        int maxJ = Integer.MIN_VALUE;
+
+        for (int i = 0; i < towers.length; i++) {
+            minI = Math.min(towers[i][0], minI);
+            maxI = Math.max(towers[i][0], maxI);
+            minJ = Math.min(towers[i][1], minJ);
+            maxJ = Math.max(towers[i][1], maxJ);
+        }
+
+        int rMinI = minI - radius;
+        int rMaxI = maxI + radius;
+        int rMinJ = minJ - radius;
+        int rMaxJ = maxJ + radius;
+
+        int maxQ = Integer.MIN_VALUE;
+        int[] result = new int[2];
+
+        for (int i = rMinI; i <= rMaxI; i++) {
+            for (int j = rMinJ; j <= rMaxJ; j++) {
+                int q = calQ(towers, i, j, radius);
+                if (q > maxQ) {
+                    result[0] = i;
+                    result[1] = j;
+                    maxQ = q;
+                }
+            }
+        }
+        return result;
+    }
+
+    private int calQ(int[][] towers, int x, int y, int radius) {
+        int q = 0;
+        for (int i = 0; i < towers.length; i++) {
+            q += signal(towers[i][0], towers[i][1], towers[i][2], x, y, radius);
+        }
+
+        return q;
+    }
+
+    private int signal(int i, int i1, int q, int x, int y, int radius) {
+
+        int d = (x - i) * (x - i) + (y - i1) * (y - i1);
+        int m = radius * radius;
+        if (d <= m) {
+            return (int) Math.floor(q / (1 + Math.sqrt(d)));
+        }
+
+        return 0;
+    }
+
 
 }
